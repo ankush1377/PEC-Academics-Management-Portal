52a815b5d4caac207a9241d975ae936a7aef012f
ankush1377 <ankush1377@yahoo.co.in>
2017-03-24 15:36:49 +0530

    Update studentCtrl.js

--- js/controllers/studentCtrl.js
+++ js/controllers/studentCtrl.js
@@ -1,37 +1,84 @@
 'use strict';
 
 pamp.controller('studentCtrl',['$scope','$rootScope','$location','$route','$http',function($scope,$rootScope,$location,$route,$http){
-	
-	var enrolledSubjectData ={
-		"sid": $rootScope.userId,
-		"semId": $rootScope.currentSemId
-	};
-	console.log(enrolledSubjectData);
-		
-	var enrolledSubjectRequest = $http({
-		method: "GET",
-		url: "php/fetchStudentSubjects.php",
-		data: enrolledSubjectData
-	});
 
-	enrolledSubjectRequest.then(function(response) {
-		console.log("response received");
-		if(response.data.records != "0"){
-			console.log(response.data.records);
-			$scope.enrolledSubjectList = response.data.records;
-		}
-		else if(response.data.records == "0"){
-			console.log(response.data.records);
-			//no records found	
-		}
-	});
+	
+	$scope.notEnrolled = false;
+	$scope.getEnrolledSubjects ($rootScope.userId, $rootScope.currentSemId);
 	
 	
-// 	var studentMarksData ={
-// 		"sid": $rootScope.userId,
-// 		"semId": $rootScope.currentSemId,
-// 		"subjectCode": $scope.
-// 	};
+	$scope.getEnrolledSubjects = function( sid, semId) {
+		
+		var enrolledSubjectData = {
+			"sid": sid,
+			"semId": semId
+		};
+	//	console.log(enrolledSubjectData);
+		
+		var enrolledSubjectRequest = $http({
+			method: "GET",
+			url: "php/fetchStudentSubjects.php",
+			data: enrolledSubjectData
+		});
+
+		enrolledSubjectRequest.then( function(response) {
+			console.log("response received");
+			if (response.data.records != "0") {
+				console.log(response.data.records);
+				$scope.enrolledSubjectList = response.data.records;
+			}
+			else if (response.data.records == "0") {
+				console.log(response.data.records);
+				$scope.notEnrolled = true;
+				//no records found	
+			}
+			else {
+				
+			}
+		});	
+	};
+
+	
+	$scope.getRelativeMarks ($rootScope.currentSemId, $scope.enrolledSubjectList[0]['subCode']);
+
+	
+	$scope.getRelativeMarks = function (semId, subjectCode){
+
+		var relativeMarksData = {
+			"semId": semId,
+			"subjectCode": subjectCode
+		};
+	//	console.log(enrolledSubjectData);
+		
+		var relativeMarksRequest = $http({
+			method: "GET",
+			url: "php/fetchRelativeMarks.php",
+			data: relativeMarksData
+		});
+
+		relativeMarksRequest.then( function(response) {
+			console.log("response received");
+			if (response.data.records != "0") {
+				console.log(response.data.records);
+				$scope.relativeMarksList = response.data.records;
+				}
+			else if (response.data.records == "0") {
+				console.log(response.data.records);
+				//no records found	
+			}
+			else {
+				
+			}
+		});	
+	};
+	
+			
+	
+ 	var studentMarksData ={
+ 		"sid": $rootScope.userId,
+ 		"semId": $rootScope.currentSemId,
+ 		"subjectCode": $scope.
+ 	};
 // 	console.log(data);
 		
 // 	var enrolledSubjectRequest = $http({
@@ -51,6 +98,17 @@ pamp.controller('studentCtrl',['$scope','$rootScope','$location','$route','$http
 // 			//no records found	
 // 		}
 // 	});
+
+	var relativePerformanceData = {
+ 		"sid": $rootScope.userId,
+ 		"semId": $rootScope.currentSemId,
+ 		"subjectCode": $scope.
+ 	};
+	
+	
+	$scope.setMyPerformance = function(semCode) {
+		
+	}
  
 	
 	
@@ -63,4 +121,3 @@ pamp.controller('studentCtrl',['$scope','$rootScope','$location','$route','$http
   
 
 }]);
-
