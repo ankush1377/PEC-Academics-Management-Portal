52684362142c902c93df9716e80aecf3c933f871
ankush1377 <ankush1377@yahoo.co.in>
2017-03-24 10:52:06 +0530

    Update fetchStudentSubjects.php

--- php/fetchStudentSubjects.php
+++ php/fetchStudentSubjects.php
@@ -18,28 +18,28 @@
 	$outp = "";
 	
 	
-	$subjectListSql = "SELECT * FROM student_subjects WHERE sem_id = '$semId' AND sid = '$sid'";
+	$subjectListSql = "SELECT * FROM student_subjects WHERE sem_id = '$semId' AND sid = '$sid' ";
 	$subjectListResult = $conn->query($subjectListSql);
 		
 	
 	if ($subjectListResult->num_rows > 0) {
 		while($row = $subjectListResult->fetch_array(MYSQLI_ASSOC)) {
-			for ($x = 1; $x <= 3; $x++) {
-				$subjectSql = "Select * FROM subject_info WHERE sub_code = '$row['subject' . "'$x'"]' ";
+			for ($x = 1; $x <= 7; $x++) {
+				$subjectSql = "Select * FROM subject_info WHERE sub_code = '$row[ 'subject' .  '$x' ]' ";
 				$subjectResult = $conn->query($subjectSql);
 				if ($subjectResult->num_rows > 0) {
 					$subjectInfo = $subjectResult->fetch_array(MYSQLI_ASSOC);
-					if($x == 3)
-						$outp .= '"' . $subjectInfo["name"] . '"';
+					if ($x == 7)
+						$outp .= $subjectInfo["name"] . '"';
 					else
-						$outp .= '"' . $subjectInfo["name"] . '",';
+						$outp .= $subjectInfo["name"] . '",';
 				}
 			} 
 		}		
-		$outp ='{"records":[' . $outp . ']}';
+		$outp ='{ "records" : [' . $outp . '] }';
 	}
 	else {
-		$outp ='{"records":"0"}';
+		$outp ='{ "records" : "0" }';
 	}
 	
 	
