3f6689af3f316a91aa29fd1af34a3adf3bbda022
ankush1377 <ankush1377@yahoo.co.in>
2017-03-29 12:13:07 +0530

    teacher

--- js/controllers/teacherCtrl.js
+++ js/controllers/teacherCtrl.js
@@ -3,6 +3,7 @@
 pamp.controller('teacherCtrl',['$scope','$rootScope','$location','$route','$http',function($scope,$rootScope,$location,$route,$http){
 
 
+	var initial = 0;
 	$scope.getAssignedSubjects = function( tid, semId) {
 		var assignedSubjectData = {
 			"tid": tid,
@@ -18,12 +19,19 @@ pamp.controller('teacherCtrl',['$scope','$rootScope','$location','$route','$http
 		assignedSubjectRequest.then( function(response) {
 			if (response.data.records != "0") {
 				$scope.assignedSubjectList_profile = response.data.records;
+                if(initial == 0){
+				    $scope.assignedSubjectList_plan = response.data.records;
+                    $scope.assignedSubjectList_assign = response.data.records;
+
+				}
+				initial++;
 //				console.log($scope.assignedSubjectList_profile);
 			}
 			else if (response.data.records == "0") {
 //				console.log(response.data.records);
-				$scope.assignedSubjectList_profile = response.data.records;
-				//no records found
+				$scope.assignedSubjectList_plan = response.data.records;
+				$scope.assignedSubjectList_assign = response.data.records;
+                //no records found
 			}
 			else {
 
